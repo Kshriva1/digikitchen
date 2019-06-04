40aebb2bf0423314efe73584913a8f154e6c5b4d
kshriva1 <kundan0395@gmail.com>
2019-06-04 18:51:12 -0400

    add signin route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Menu from './Components/Menu';
 import ModifyOrder from './Components/ModifyOrder';
 import Kitchen from './Components/Kitchen';
+import Register from './Components/Register';
+import SignIn from './Components/SignIn';
 import {
   Collapse,
   Navbar,
@@ -19,7 +21,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       isOpen: false,
-      route: 'menu'
+      route: 'register',
+      isSignedIn: false
     };
   }
 
@@ -30,22 +33,34 @@ class App extends React.Component {
   }
 
   onRouteChange = (route) => {
-  	this.setState({
-      route: route
-  	});
+  	if(route === "signout") {
+      this.setState({
+        isSignedIn: false,
+        route: 'register'
+      })
+    } else if(route === "home") {
+      this.setState({
+        isSignedIn: true,
+        route: 'menu'
+      })
+    } else {
+      this.setState({route: route})
+    }
   }
 
+
   render() {
-  	console.log(this.state.route);
   return (
     <div>
+      {this.state.isSignedIn === true ?
+        <div>
     <Navbar color="light-red" light expand="md">
           <NavbarBrand style={{cursor: 'pointer'}}>Welcome to Chef!</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
                <NavItem>
-                <NavLink style={{cursor: 'pointer',paddingRight:'15px',textDecoration:'underline'}} onClick={() => this.onRouteChange('home')}>Sign Out</NavLink>
+                <NavLink style={{cursor: 'pointer',paddingRight:'15px',textDecoration:'underline'}} onClick={() => this.onRouteChange('signout')}>Sign Out</NavLink>
               </NavItem>
               <NavItem>
                 <NavLink style={{cursor: 'pointer'}} onClick={() => this.onRouteChange('menu')}>Menu</NavLink>
@@ -82,8 +97,34 @@ class App extends React.Component {
     KITCHEN AREA
     </div>
     <Kitchen />  
-    </div>)
-    }
+    </div>)}
+    </div>
+     : 
+     <div>
+     <Navbar color="light-red" light expand="md">
+          <NavbarBrand style={{cursor: 'pointer'}}>Welcome to Chef!</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+               <NavItem>
+                <NavLink style={{cursor: 'pointer',paddingRight:'15px',textDecoration:'underline'}} onClick={() => this.onRouteChange('signin')}>Sign In</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={{cursor: 'pointer',paddingRight:'15px',textDecoration:'underline'}} onClick={() => this.onRouteChange('register')}>Register</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+    <div className='pa3'>
+    <div className='navigation pa3  bg-light-red' style={{height:'100px',width:'100px'}}>
+        <img src={kitchenLogo} className="pointer App-logo" alt="logo" />
+    </div>
+    </div>
+     {this.state.route === 'register' ?
+      <div>Register</div> : <div>SignIn</div>
+     }
+     </div>
+      }
     </div>
   );
  }
